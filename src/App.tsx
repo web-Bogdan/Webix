23a9a1c82f78de84a5e4615cdd0be21fc6533283
web-Bogdan <madare78@mail.ru>
2022-04-29 19:12:28 +0300

    Создал роутинг

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
-
-import Auth from "./pages/Auth";
+import {Routes, Route} from "react-router";
+import {publicRoutes} from "./routes/routes";
 function App() {
-  return (
-      <div className="App">
-        <CssBaseline />
-          <Auth/>
-      </div>
-  );
+    return (
+        <div className="App">
+            <CssBaseline />
+            <Routes>
+                {publicRoutes.map(route => (
+                    <Route key={route.path} path={route.path} element={route.element}/>
+                ))}
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/routes/routes.tsx
@@ -0,0 +1,8 @@
+
+import Auth from "../pages/Auth";
+import {RouteObject} from "react-router";
+
+
+export const publicRoutes: RouteObject[] = [
+    {path: "/auth", element: <Auth/>}
+]
